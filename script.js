024ff20b97c28babbe683c23b2f6a20b7e9a933e
arapimana <129587248+arapimana@users.noreply.github.com>
2024-07-03 00:59:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function checkAnswer() {
     const answerInput = document.getElementById('answerInput').value;
     const result = document.getElementById('result');
 
-    if (answerInput === "解決") {
-        result.textContent = "おめでとう";
+    if (answerInput === "俺はお兄様だぞ") {
+        result.textContent = "や、やるじゃない、、、";
     } else {
-        result.textContent = "残念";
+        result.textContent = "ざぁこ♡ざぁこ♡";
     }
 }
